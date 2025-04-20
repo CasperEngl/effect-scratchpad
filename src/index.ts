abe3ec100a8bbf17f431370e2215ed287078dfe7
Casper Engelmann <me@casperengelmann.com>
2025-04-20 22:36:21 +0200

    ♻️ refactor(tracing): use Tracing Context.Tag

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Effect, pipe } from "effect";
 import { Database } from "./db/db";
 import { getUsers } from "./db/get-users";
-import { TracingLive } from "./tracing";
+import { Tracing } from "./tracing";
 
 const result = await pipe(
 	getUsers,
 	Effect.provide(Database.Live),
-	Effect.provide(TracingLive),
+	Effect.provide(Tracing.Live),
 	Effect.runPromise,
 );
 
--- src/tracing.ts
+++ src/tracing.ts
@@ -1,25 +1,30 @@
-import { NodeSdk } from "@effect/opentelemetry";
+import { NodeSdk, type Resource } from "@effect/opentelemetry";
 import {
 	BatchSpanProcessor,
 	ConsoleSpanExporter,
 } from "@opentelemetry/sdk-trace-base";
+import { Context } from "effect";
 
-export const TracingLive = NodeSdk.layer(() => ({
-	resource: {
-		serviceName: "effect-app",
-		attributes: {
-			environment: "production",
+export class Tracing extends Context.Tag("Tracing")<
+	Tracing,
+	typeof Resource.Resource
+>() {
+	static readonly Live = NodeSdk.layer(() => ({
+		resource: {
+			serviceName: "effect-app",
+			attributes: {
+				environment: "production",
+			},
 		},
-	},
-	spanProcessor: new BatchSpanProcessor(new ConsoleSpanExporter()),
-}));
-
-export const TracingTest = NodeSdk.layer(() => ({
-	resource: {
-		serviceName: "effect-app",
-		attributes: {
-			environment: "test",
+		spanProcessor: new BatchSpanProcessor(new ConsoleSpanExporter()),
+	}));
+	static readonly Test = NodeSdk.layer(() => ({
+		resource: {
+			serviceName: "effect-app",
+			attributes: {
+				environment: "test",
+			},
 		},
-	},
-	spanProcessor: new BatchSpanProcessor(new ConsoleSpanExporter()),
-}));
+		spanProcessor: new BatchSpanProcessor(new ConsoleSpanExporter()),
+	}));
+}
